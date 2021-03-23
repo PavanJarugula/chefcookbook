9fbdec86d635a6f00bbb5aa1e9f2fa2d0d4587e2
root <root@ip-172-31-16-227.us-east-2.compute.internal>
2021-03-23 19:27:45 +0000

    Cookbooks of chef

---
+++ apache-cookbook/recipes/apache-recipe.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: apache-cookbook
+# Recipe:: apache-recipe
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+
+file '/var/www/html/index.html' do
+  content 'Hello Dear students!!!,How are you?'
+  action :create
+end
+
+
+service 'httpd' do
+  action [ :enable, :start]
+end
+
---
+++ test-cookbook/recipes/demo-recipe.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook:: test-cookbook
+# Recipe:: demo-recipe
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
+package 'tree' do
+  action:install
+end
+
+file '/myfile2' do
+  content 'This is my 2nd file'
+  action:create
+  owner 'root'
+  group 'root'
+end
+
+%w(httpd mariadb-server unzip git vim).each do |p|
+  package p do
+    action :install
+  end
+end
